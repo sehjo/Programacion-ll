1752b1d10a448566d82ca681632e5bb503601a97
Usuario <Usuario@SPLab1-11>
2024-03-15 08:36:22 -0600

    primera carga

---
+++ src/main/java/ucr/ac/cr/programacion/ll/ProgramacionLl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ucr.ac.cr.programacion.ll;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ProgramacionLl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
